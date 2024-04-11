b2a04d02ded334eec22c96ab0065998942b7cc8e
rhugvedd <rhugved.c@gmail.com>
2024-04-12 01:40:11 +0530

    NumberPlateDetector class moved to its dedicated file. Checkpoint mechanism updated. Now loading of checkpoints also possible and other minor modifications done.

--- Train.py
+++ Train.py
@@ -1,55 +1,28 @@
-import torch
-import torch.nn as nn
-import torchvision
-import torchvision.transforms as transforms
-import torch.optim as optim
-import time
-from datetime import datetime
-
-from DataExtractor import *
-from Darknet import *
-from YoloLayer import *
-from YoloLoss import *
-
-class NumberPlateDetector(nn.Module):
-    def __init__(self, image_size, scaling_fact, anchors, device):
-        super(NumberPlateDetector, self).__init__()
-
-        self.anchor_nos = len(anchors)
-
-        self.Darknet53 = CSPDarknet(in_dim=image_size, training=True)
-        self.YoloConv = YoloConv(in_channels=512, hidden_channels=1024, out_channels=512)
-        self.YoloHead = YoloHead(anchors.to(device), num_classes=0, scaling_fact=torch.tensor(scaling_fact).to(device), in_channels=512)
-        self.YoloLoss = YoloLoss(lambda_cord=5, lambda_obj=None, lambda_no_obj=0.5)
-
-    def forward(self, X, Y):
-        X = self.Darknet53(X)
-        X = self.YoloConv(X)
-
-        gt_anchor_idx = torch.argmax(Y.any(-1).type(torch.int), dim = 3)
-        
-        X = self.YoloHead(X, gt_anchor_idx)
-
-        loss = self.YoloLoss(X, Y, self.anchor_nos)
-
-        return loss
+from NumberPlateDetector import *
 
 """
 List of Hyperparameters
 """
-model_name = 'NumPlate-5000'
+model_name = 'NumPlate-Final-5000-Pre-Train-LR-0.0001'
 checkpoint_path = './CheckPoints/'
+load_check_point = True
+checkpoint_name = 'NumPlate-Final-5000-Pre-Train-Epoch-10-2024-04-11' + '.pth'
 data_path = "NumPlateData/"
+X_Name = 'X_Train_9-2024-04-11 22-53-24'
+Y_Name = 'Y_Train_9-2024-04-11 22-53-24'
+anchor_name = 'Anchor_train_9'
 image_scale = 9
 image_size = (image_scale*32, image_scale*32)
 scaling_fact = 32
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-batch_size = 48
+batch_size = 12
+norm_momentum = 0.075
 num_examples = 5000
-num_epochs = 100
+num_epochs = 1000
 loss_eval_interval = 0
 loss_eval_interval += 1
-checkpoint_save_epoch = 1
+checkpoint_save_epoch = 5
+learning_rate = 0.0001
 """
 List ends.
 """
@@ -65,25 +38,52 @@ TrainDataExtractor = DataExtractor  (
                                         device=device
                                     )
 
-anchors = TrainDataExtractor.getAnchors_fromMem('Anchor_train')
+anchors = TrainDataExtractor.getAnchors_fromMem(anchor_name)
 anchor_nos = anchors.size(0)
 
-X_train, Y_train = TrainDataExtractor.getXY_fromMem(X_Name='X_Train-2024-04-09 19-12-15', Y_Name='Y_Train-2024-04-09 19-12-15')[:num_examples]
+X_train, Y_train = TrainDataExtractor.getXY_fromMem(X_Name=X_Name, Y_Name=Y_Name)[:num_examples]
 
-NumPlatesTrainer = NumberPlateDetector(image_size, scaling_fact, anchors, device)
+# to_pil_image(X_train[0]).save('X_2.jpg')
+
+NumPlatesTrainer = NumberPlateDetector(image_size, scaling_fact, anchors, device, norm_momentum)
 NumPlatesTrainer.to(device)
+optimizer = optim.Adam(NumPlatesTrainer.parameters(), lr=learning_rate)
 
-optimizer = optim.Adam(NumPlatesTrainer.parameters(), lr=0.001)
+st_epoch = 0
+if load_check_point:
+    checkpoint = torch.load(checkpoint_path + checkpoint_name)
+    NumPlatesTrainer.load_state_dict(checkpoint['model_state_dict'])   
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    
+    print('\n\nLoaded Checkpoint: ' + checkpoint_path + checkpoint_name)
+    
+    lr = optimizer.param_groups[0]['lr']
+    print("Learning rate of loaded model:", lr)
+    
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = learning_rate
+    
+    print("Setting learning rate to:", optimizer.param_groups[0]['lr'])
+    
+    st_epoch = checkpoint['epoch'] + 1
+    loss = checkpoint['loss']
+
+    print(f'Starting Epoch for Training: {st_epoch}')
+
 num_batches = len(X_train) // batch_size
 
 start_time = time.time()
 
-print("Computing Started")
+print("Training Started")
 
-for epoch in range(num_epochs + 1):
+TotalLoss_List = []
+
+for epoch in range(st_epoch, num_epochs + 1):
     running_loss = 0.0
     epoch_loss = 0.0
     
+    TotalEpoch_List = []
+
     epoch_st_time = time.time()
     
     for i in range(num_batches):
@@ -92,15 +92,21 @@ for epoch in range(num_epochs + 1):
         Y_batch = Y_train[i * batch_size : (i + 1) * batch_size].to(device).float()
 
         optimizer.zero_grad()
-        loss = NumPlatesTrainer(X_batch, Y_batch)
+        loss = NumPlatesTrainer.train_data(X_batch, Y_batch)
         running_loss += loss
         epoch_loss += loss
         loss.backward()
         optimizer.step()
 
+        TotalEpoch_List.append(loss)
+
         if i % loss_eval_interval == 0:
-            print('[Epoch: %d, Batch: %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / loss_eval_interval))
+            print('[Epoch: %d, Batch: %5d] loss: %.3f' % (epoch, i + 1, running_loss / loss_eval_interval))
             running_loss = 0.0
+
+    TotalLoss_List.append(TotalEpoch_List)
+
+    print(TotalLoss_List)
 
     if (epoch % checkpoint_save_epoch == 0) and (epoch != 0):
         date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S").replace(':', '-')
@@ -109,9 +115,10 @@ for epoch in range(num_epochs + 1):
                         'epoch': epoch,
                         'model_state_dict': NumPlatesTrainer.state_dict(),
                         'optimizer_state_dict': optimizer.state_dict(),
-                        'loss': loss,
+                        'loss': TotalLoss_List,
                     }, checkpoint_path + model_name + '-Epoch-' + str(epoch) + '-' + date_time + '.pth')
         
+        TotalLoss_List = []
         print("Checkpoint Saved")
     
     print(f"Time Taken Epoch: {epoch} - {time.time() - epoch_st_time:.2f} seconds")
